5aca8c610743c7e011f0eedf6a8ea979c87e6733
FLY-CODE77 <lightyear94122@gmail.com>
2021-02-23 14:52:23 +0900

    Code : main1.py

--- class_basic/main.py
+++ class_basic/main.py
@@ -1,3 +1,24 @@
 import hello
 
 print('main.py __name__:', __name__)
+
+# hello.py
+
+# print('hello start')
+# print('hello.py __name__:', __name__)
+# print('hello end')
+
+# result 
+
+# hello start
+# hello.py __name__: hello
+# hello end
+# main.py __name__: __main__
+
+# check when hello.py import !
+# hello.py __name __ print hello
+# and main.py __name__ is __main__
+
+# if __name__ == '__main__' means 
+# run code !! when start point! (*origin?)
+
