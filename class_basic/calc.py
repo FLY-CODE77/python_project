524ff2ecaf0b95c60f35cf3815b824d196cf6da2
FLY-CODE77 <lightyear94122@gmail.com>
2021-02-23 14:50:32 +0900

    Code : calc.py

---
+++ class_basic/calc.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(add(10, 20))
+    print(mul(10, 20))
+
+# print(add(10 ,20)) 
+# when file is calc.py
+
